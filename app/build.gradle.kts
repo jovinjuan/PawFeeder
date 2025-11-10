33dd0ce3becfe7f3a71212d5a5e3732740999cd2
jefva <68883816+Jvm9k@users.noreply.github.com>
2025-11-10 19:23:42 +0700

    Working on Schedule Fragment UI
    dont touch (to do).
    a lot of errors.
    ui doesnt work for now, but will be fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.database)
     implementation(libs.firebase.storage)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
