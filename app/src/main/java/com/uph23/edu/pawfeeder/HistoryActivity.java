36b7d0905c2262b5269de87cda94f6d919bd13ec
jovinjuan <jovinjuanlie05@gmail.com>
2026-03-14 15:48:02 +0700

    Add history model

--- app/src/main/java/com/uph23/edu/pawfeeder/HistoryActivity.java
+++ app/src/main/java/com/uph23/edu/pawfeeder/HistoryActivity.java
@@ -1,15 +1,23 @@
 package com.uph23.edu.pawfeeder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class HistoryActivity extends AppCompatActivity {
 
+    ImageView imgBack, imgFilter;
+    TextView txvTotalSchedule, txvFood, txvMissed, txvTodayDate, txvYesterdayDate;
+    RecyclerView rvTodaySchedule, rvYesterdaySchedule;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,23 @@ public class HistoryActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+    public void init(){
+        imgBack = findViewById(R.id.imgBack);
+        imgFilter = findViewById(R.id.imgFilter);
+        txvTotalSchedule = findViewById(R.id.txvTotalSchedule);
+        txvFood = findViewById(R.id.txvFood);
+        txvMissed = findViewById(R.id.txvMissed);
+        txvTodayDate = findViewById(R.id.txvTodayDate);
+        txvYesterdayDate = findViewById(R.id.txvYesterdayDate);
+        rvTodaySchedule = findViewById(R.id.rvTodaySchedule);
+        rvYesterdaySchedule = findViewById(R.id.rvYesterdaySchedule);
     }
 }
---
+++ app/src/main/java/com/uph23/edu/pawfeeder/model/History.java
@@ -0,0 +1,44 @@
+package com.uph23.edu.pawfeeder.model;
+
+public class History {
+    private String title, mode, description, status;
+
+    public History(String title, String mode, String description, String status) {
+        this.title = title;
+        this.mode = mode;
+        this.description = description;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
